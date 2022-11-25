8f53ce62fddf8e1979498daf8270becd3d3d3c58
Nicolas Fernandez Riesen <riesenfnicolas@gmail.com>
2022-11-25 16:28:28 -0300

    Agregar archivos de proyecto.

---
+++ PhotoSharingApplication/Data/PhotoDBContext.cs
@@ -0,0 +1,18 @@
+﻿using PhotoSharingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Data
+{
+    public class PhotoDBContext : DbContext
+    {
+        public PhotoDBContext() : base("keyPhotoSharingApplicationDB") { }
+
+        public DbSet<Photo> Photos { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ PhotoSharingApplication/Models/Photo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoSharingApplication.Models
+{
+    public class Photo
+    {
+        public int PhotoID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [DisplayName("Picture")]
+        [MaxLength]
+
+        public byte[] PhotoFile { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public string ImageMimeType { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Created Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+        public string UserName { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
